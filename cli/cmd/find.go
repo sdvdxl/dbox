67de47b9c82e437dc9bcc24b698b457b3a302ccc
sdvdxl杜龙少 <sdvdxl@163.com>
2019-05-04 10:49:17 +0800

    优化查找
    Signed-off-by: sdvdxl杜龙少 <sdvdxl@163.com>

--- cli/cmd/find.go
+++ cli/cmd/find.go
@@ -29,27 +29,21 @@ import (
 // findCmd represents the find command
 var findCmd = &cobra.Command{
 	Use:   "find",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "search files",
+	Args:  cobra.MinimumNArgs(1),
+	
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.PersistentFlags().VisitAll(func(flag *pflag.Flag) {
 			log.Log.Info(flag.Name, " ", flag.Value)
+			log.Log.Info("args", args)
 		})
 
 		checkConfig()
-		if findCondition.Name == "" {
-			cmd.Help()
-			os.Exit(1)
-		}
+		findCondition.Name = args[0]
 
-		fileService:=&service.FileService{}
+		fileService := &service.FileService{}
 
-		files:=fileService.FindByFuzz(findCondition)
+		files := fileService.FindByFuzz(findCondition)
 		printTables(files)
 	},
 }
@@ -83,7 +77,6 @@ var (
 func init() {
 	rootCmd.AddCommand(findCmd)
 	findCmd.PersistentFlags().StringVarP(&findCondition.Category, "category", "c", "", "文件夹")
-	findCmd.PersistentFlags().StringVarP(&findCondition.Name, "file", "f", "", "* 文件名字")
 
 	// Here you will define your flags and configuration settings.
 
